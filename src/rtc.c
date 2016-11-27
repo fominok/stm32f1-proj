a7302f0d16c9b33ebf4369509f4146e18b5d9318
Evgeny Fomin <fominok@hotmail.com>
2016-11-27 20:41:12 +0300

    Fix I2C N > 2 bytes receiver

--- src/rtc.c
+++ src/rtc.c
@@ -29,14 +29,15 @@ void read_rtc_time(uint8_t *second, uint8_t *minute, uint8_t *hour,
   I2C_WriteData(I2C1, 0);
   I2C_GenerateSTOP(I2C1, ENABLE);
   I2C_StartTransmission(I2C1, I2C_Direction_Receiver, 0x68);
+  I2C_AcknowledgeConfig(I2C1, ENABLE);
   *second = bcd_to_dec(I2C_ReadData(I2C1) & 0x7f);
   *minute = bcd_to_dec(I2C_ReadData(I2C1));
   *hour = bcd_to_dec(I2C_ReadData(I2C1) & 0x3f);
   *dayOfWeek = bcd_to_dec(I2C_ReadData(I2C1));
   *dayOfMonth = bcd_to_dec(I2C_ReadData(I2C1));
   *month = bcd_to_dec(I2C_ReadData(I2C1));
-  *year = bcd_to_dec(I2C_ReadData(I2C1));
   I2C_AcknowledgeConfig(I2C1, DISABLE);
+  *year = bcd_to_dec(I2C_ReadData(I2C1));
   I2C_GenerateSTOP(I2C1, ENABLE);
 }
 
